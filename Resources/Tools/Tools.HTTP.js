ae187fd6304553528e3d8b2d23b39791146773b7
Trifonov Alexander <fgengine@gmail.com>
2012-07-23 10:18:38 +0400

    Performing requests by Ti.Network.HTTPClient in the order of queue

--- Resources/Tools/Tools.HTTP.js
+++ Resources/Tools/Tools.HTTP.js
@@ -1,71 +1,104 @@
-var online = function()
+//---------------------------------------------//
+
+if(Ti.App.ToolsHttp == undefined)
+{
+	Ti.App.ToolsHttp = {
+		handle : undefined,
+		queue : []
+	};
+}
+
+//---------------------------------------------//
+
+function online()
 {
 	return Ti.Network.online;
 }
 
-var response = function(params)
+function response(params)
 {
-	var http = Ti.Network.createHTTPClient(
-		{
-			enableKeepAlive : false,
-			timeout : 30000,
-			onload : function()
-			{
-				if(params.success != undefined)
-				{
-					params.success(http);
-				}
-				http = null;
-			},
-			onerror : function()
-			{
-				if(params.failure != undefined)
-				{
-					params.failure(http.status);
-				}
-				http = null;
-			}
-		}
-	);
-	if(params.header != undefined)
+	if(params == undefined)
 	{
-		for(var i = 0; i < params.header.length; i++)
-		{
-			http.getdata.setRequestHeader(params.header[i].type, params.header[i].value);
-		}
+		throw new Error(L('TI_TOOLS_THROW_BAD_PARAMS'));
 	}
-	switch(params.method)
+	if(Ti.App.ToolsHttp.handle == undefined)
 	{
-		case 'GET':
-			var url = params.url;
-			if(params.data != undefined)
+		Ti.App.ToolsHttp.handle = Ti.Network.createHTTPClient(
 			{
-				var count = 0;
-				for(var i in params.data)
+				enableKeepAlive : false,
+				timeout : 30000,
+				onload : function()
 				{
-					if(params.data[i] != undefined)
+					if(params.success != undefined)
 					{
-						if(count == 0)
-						{
-							url += '?' + i + '=' + params.data[i];
-						}
-						else
-						{
-							url += '&' + i + '=' + params.data[i];
-						}
-						count++;
+						params.success(http);
+					}
+					Ti.App.ToolsHttp.queue.splice(0, 1);
+					Ti.App.ToolsHttp.handle = undefined;
+					if(Ti.App.ToolsHttp.queue.length > 0)
+					{
+						response(Ti.App.ToolsHttp.queue[0]);
+					}
+				},
+				onerror : function()
+				{
+					if(params.failure != undefined)
+					{
+						params.failure(http.status);
+					}
+					Ti.App.ToolsHttp.queue.splice(0, 1);
+					Ti.App.ToolsHttp.handle = undefined;
+					if(Ti.App.ToolsHttp.queue.length > 0)
+					{
+						response(Ti.App.ToolsHttp.queue[0]);
 					}
 				}
 			}
-			http.getdata.open('GET', url);
-			http.getdata.send();
-		break;
-		case 'POST':
-			http.getdata.open('POST', params.url);
-			http.getdata.send(params.data);
-		break;
-		default:
-			throw new Error(L('TI_TOOLS_THROW_UNKNOWN_METHOD'));
+		);
+		if(params.header != undefined)
+		{
+			for(var i = 0; i < params.header.length; i++)
+			{
+				http.getdata.setRequestHeader(params.header[i].type, params.header[i].value);
+			}
+		}
+		switch(params.method)
+		{
+			case 'GET':
+				var url = params.url;
+				if(params.data != undefined)
+				{
+					var count = 0;
+					for(var i in params.data)
+					{
+						if(params.data[i] != undefined)
+						{
+							if(count == 0)
+							{
+								url += '?' + i + '=' + params.data[i];
+							}
+							else
+							{
+								url += '&' + i + '=' + params.data[i];
+							}
+							count++;
+						}
+					}
+				}
+				http.getdata.open('GET', url);
+				http.getdata.send();
+			break;
+			case 'POST':
+				http.getdata.open('POST', params.url);
+				http.getdata.send(params.data);
+			break;
+			default:
+				throw new Error(L('TI_TOOLS_THROW_UNKNOWN_METHOD'));
+		}
+	}
+	else
+	{
+		Ti.App.ToolsHttp.queue.push(params);
 	}
 };
 
